d6a0cbfa7d11e30973400d8d16fe8a4cf37de03e
苍葵 <15148395+cangkui6@user.noreply.gitee.com>
2025-04-24 14:20:04 +0800

    实现了随机轮询

--- service-consumer_8083/src/main/java/com/example/consumer/ServiceConsumerApplication.java
+++ service-consumer_8083/src/main/java/com/example/consumer/ServiceConsumerApplication.java
@@ -1,9 +1,11 @@
 package com.example.consumer;
 
+import com.example.consumer.config.LoadBalancerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient  // 启用 Eureka 客户端
 @EnableFeignClients  // 启用 OpenFeign
+@LoadBalancerClient(name = "SERVICE-PROVIDER", configuration = LoadBalancerConfig.class)
 public class ServiceConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceConsumerApplication.class, args);
